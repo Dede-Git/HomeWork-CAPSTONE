bc5819fa1f02b700e195e2ecd0f3c601e15cb8bd
DeAndre Hill <deandre.hill2001@gmail.com>
2023-03-13 21:25:28 -0500

    refactored my navbar and merged some code to get rid of unessasary code

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { getPlans } from '../api/planData';
 import PlanCard from '../components/PlanCard';
-import UserProfile from '../components/UserProfile';
+import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
 export default function ProfilePage() {
@@ -18,7 +19,15 @@ export default function ProfilePage() {
   }, []);
   return (
     <>
-      <div><UserProfile /></div>
+      <div id="profileSection">
+        <img src={user.photoURL} alt="userURL" width="200px" height="200px" id="profilepicture" />
+        <h2>{user.displayName}</h2>
+        <h5>{user.email}</h5>
+        <h5>Last Login: {user.metadata.lastSignInTime}</h5>
+        <Button type="button" size="lg" cvariant="outline-dark" className="m-2" onClick={signOut}>
+          Sign Out
+        </Button>
+      </div>
       <div className="d-flex flex-wrap" id="profileworkouts">
         {plans.map((plan) => (
           <PlanCard key={plan.firebaseKey} planObj={plan} onUpdate={getAllThePlans} />
